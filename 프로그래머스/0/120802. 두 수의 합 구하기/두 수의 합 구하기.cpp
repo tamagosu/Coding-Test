c1da3147fa51795954d8b89d9d7f94adebc72ecb
Jiwoo <148704530+tamagosu@users.noreply.github.com>
2025-03-28 13:22:48 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+    cout << solution(2, 3) << endl;
+    cout << solution(100, 2) << endl;
+    return 0;
+}
