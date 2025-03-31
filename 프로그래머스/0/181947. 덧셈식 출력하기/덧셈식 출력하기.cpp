5bff3c983a8c6534e0068e390b0a10ac9e336d75
Jiwoo <148704530+tamagosu@users.noreply.github.com>
2025-03-31 23:35:25 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 3.97 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+    
+    int c = a + b;
+    cout << a << " + " << b << " = " << c << endl;
+    
+    return 0;
+}
